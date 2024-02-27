f35476ed12677faa081f4280954361ab26e20baa
compyx <b.wassink@ziggo.nl>
2024-02-27 09:28:58 +0100

    Linux: use `libevdev_event_code_get_name() for button and axis names
    Use own function for hat names since evdev doesn't have actual hats,
    just two axes comprising a hat (e.g. `ABS_HAT0X` + `ABS_HAT0Y`).

--- src/linux/joy.c
+++ src/linux/joy.c
@@ -94,96 +94,6 @@ typedef struct joy_priv_s {
 #define IS_HAT(code) ((code) >= ABS_HAT0X) && ((code) <= ABS_HAT3Y)
 
 
-/** \brief  Button names */
-static const ev_code_name_t button_names[] = {
-    /* 0x100-0x109 - BTN_MISC */
-    { BTN_0,        "Btn0" },           { BTN_1,        "Btn1" },
-    { BTN_0,        "Btn2" },           { BTN_1,        "Btn3" },
-    { BTN_0,        "Btn4" },           { BTN_1,        "Btn5" },
-    { BTN_0,        "Btn6" },           { BTN_1,        "Btn7" },
-    { BTN_0,        "Btn8" },           { BTN_1,        "Btn9" },
-
-    /* 0x110-0x117 - BTN_MOUSE */
-    { BTN_LEFT,     "LeftBtn" },        { BTN_RIGHT,    "RightBtn" },
-    { BTN_MIDDLE,   "MiddleBtn" },      { BTN_SIDE,     "SideBtn" },
-    { BTN_EXTRA,    "ExtraBtn" },       { BTN_FORWARD,  "FowardBtn" },
-    { BTN_BACK,     "BackBtn" },        { BTN_TASK,     "TaskBtn" },
-
-    /* 0x120-0x12f - BTN_JOYSTICK */
-    { BTN_TRIGGER,  "Trigger" },        { BTN_THUMB,    "ThumbBtn" },
-    { BTN_THUMB2,   "ThumbBtn2" },      { BTN_TOP,      "TopBtn" },
-    { BTN_TOP2,     "TopBtn2" },        { BTN_PINKIE,   "PinkieButton" },
-    { BTN_BASE,     "BaseBtn" },        { BTN_BASE2,    "BaseBtn2" },
-    { BTN_BASE3,    "BaseBtn3" },       { BTN_BASE4,    "BaseBtn4" },
-    { BTN_BASE5,    "BaseBtn5" },       { BTN_BASE6,    "BaseBtn6" },
-    { BTN_DEAD,     "BtnDead" },
-
-    /* 0x130-0x13e - BTN_GAMEPAD */
-    { BTN_A,        "BtnA" },           { BTN_B,        "BtnB" },
-    { BTN_C,        "BtnC" },           { BTN_X,        "BtnX" },
-    { BTN_Y,        "BtnY" },           { BTN_Z,        "BtnZ" },
-    { BTN_TL,       "BtnTL" },          { BTN_TR,       "BtnTR" },
-    { BTN_TL2,      "BtnTL2" },         { BTN_TR2,      "BtnTR2" },
-    { BTN_SELECT,   "BtnSelect" },      { BTN_START,    "BtnStart" },
-    { BTN_MODE,     "BtnMode" },        { BTN_THUMBL,   "BtnThumbL" },
-    { BTN_THUMBR,   "BtnThumbR" },
-
-    /* Skipping 0x140-0x14f: BTN_TOOL_PEN - BTN_TOOL_QUADTAP */
-
-    /* 0x150-0x151: BTN_WHEEL */
-    { BTN_GEAR_DOWN, "GearDown" },      { BTN_GEAR_UP,  "GearUp" },
-
-    /* 0x220-0223 */
-    { BTN_DPAD_UP,   "BtnDPadUp" },     { BTN_DPAD_DOWN,  "BtnDPadDown" },
-    { BTN_DPAD_LEFT, "BtnDPadLeft" },   { BTN_DPAD_RIGHT, "BtnDPadRight" }
-};
-
-/** \brief  Axis names */
-static const ev_code_name_t axis_names[] = {
-    { ABS_X,            "X" },
-    { ABS_Y,            "Y" },
-    { ABS_Z,            "Z" },
-    { ABS_RX,           "Rx" },
-    { ABS_RY,           "Ry" },
-    { ABS_RZ,           "Rz" },
-    { ABS_THROTTLE,     "Throttle" },
-    { ABS_RUDDER,       "Rudder" },
-    { ABS_WHEEL,        "Wheel" },
-    { ABS_GAS,          "Gas" },
-    { ABS_BRAKE,        "Brake" },
-    { ABS_HAT0X,        "Hat0X" },
-    { ABS_HAT0Y,        "Hat0Y" },
-    { ABS_HAT1X,        "Hat1X" },
-    { ABS_HAT1Y,        "Hat1Y" },
-    { ABS_HAT2X,        "Hat2X" },
-    { ABS_HAT2Y,        "Hat2Y" },
-    { ABS_HAT3X,        "Hat3X" },
-    { ABS_HAT3Y,        "Hat3Y" },
-    { ABS_PRESSURE,     "Pressure" },
-    { ABS_DISTANCE,     "Distance" },
-    { ABS_TILT_X,       "XTilt" },
-    { ABS_TILT_Y,       "YTilt" },
-    { ABS_TOOL_WIDTH,   "ToolWidth" },
-    { ABS_VOLUME,       "Volume" },
-    { ABS_PROFILE,      "Profile" },
-    { ABS_MISC,         "Misc" }
-};
-
-/** \brief  Hat names
- *
- * Each of two hat axes maps to the same name.
- */
-static const ev_code_name_t hat_names[] = {
-    { ABS_HAT0X,    "Hat0" },
-    { ABS_HAT0Y,    "Hat0" },
-    { ABS_HAT1X,    "Hat1" },
-    { ABS_HAT1Y,    "Hat1" },
-    { ABS_HAT2X,    "Hat2" },
-    { ABS_HAT2Y,    "Hat2" },
-    { ABS_HAT3X,    "Hat3" },
-    { ABS_HAT3Y,    "Hat3" },
-};
-
 /** \brief X and Y axes for the four hats found in `linux/input-event-codes.h` */
 static const hat_evcode_t hat_event_codes[] = {
     { ABS_HAT0X, ABS_HAT0Y },
@@ -217,33 +127,24 @@ static void joydev_priv_free(void *priv)
     lib_free(priv);
 }
 
-static const char *get_event_code_name(const ev_code_name_t *list,
-                                       size_t                length,
-                                       unsigned int          code)
-{
-    for (size_t i = 0; i < length; i++) {
-        if (list[i].code == code) {
-            return list[i].name;
-        } else if (list[i].code > code) {
-            break;
-        }
-    }
-    return NULL;
-}
-
-static const char *get_axis_name(unsigned int code)
-{
-    return get_event_code_name(axis_names, ARRAY_LEN(axis_names), code);
-}
-
-static const char *get_button_name(unsigned int code)
-{
-    return get_event_code_name(button_names, ARRAY_LEN(button_names), code);
-}
-
 static const char *get_hat_name(unsigned int code)
 {
-    return get_event_code_name(hat_names, ARRAY_LEN(hat_names), code);
+    switch (code) {
+        case ABS_HAT0X: /* fall through */
+        case ABS_HAT0Y:
+            return "ABS_HAT0";
+        case ABS_HAT1X: /* fall through */
+        case ABS_HAT1Y:
+            return "ABS_HAT1";
+        case ABS_HAT2X: /* fall through */
+        case ABS_HAT2Y:
+            return "ABS_HAT2";
+        case ABS_HAT3X: /* fall through */
+        case ABS_HAT3Y:
+            return "ABS_HAT3";
+        default:
+            return NULL;
+    }
 }
 
 static int node_filter(const struct dirent *de)
@@ -296,7 +197,7 @@ static void scan_buttons(joy_device_t *joydev, struct libevdev *evdev)
                 button = &(joydev->buttons[num++]);
                 joy_button_init(button);
                 button->code = (uint16_t)code;
-                button->name = lib_strdup(get_button_name(code));
+                button->name = lib_strdup(libevdev_event_code_get_name(EV_KEY, code));
                 //printf("%s\n", button->name);
             }
         }
@@ -332,7 +233,7 @@ static void scan_axes(joy_device_t *joydev, struct libevdev *evdev)
                 axis    = &(joydev->axes[num]);
                 joy_axis_init(axis);
                 axis->code = (uint16_t)code;
-                axis->name = lib_strdup(get_axis_name(code));
+                axis->name = lib_strdup(libevdev_event_code_get_name(EV_ABS, code));
                 //printf("axis name = %s\n", axis->name);
                 if (absinfo != NULL) {
                     axis->minimum    = absinfo->minimum;
@@ -392,7 +293,7 @@ static void scan_hats(joy_device_t *joydev, struct libevdev *evdev)
                 /* X axis */
                 absinfo = libevdev_get_abs_info(evdev, x_code);
                 x_axis->code = x_code;
-                x_axis->name = lib_strdup(get_axis_name(x_code));
+                x_axis->name = lib_strdup(libevdev_event_code_get_name(EV_ABS, x_code));
                 if (absinfo != NULL) {
                     x_axis->minimum    = absinfo->minimum;
                     x_axis->maximum    = absinfo->maximum;
@@ -407,7 +308,7 @@ static void scan_hats(joy_device_t *joydev, struct libevdev *evdev)
                 /* Y axis */
                 absinfo = libevdev_get_abs_info(evdev, y_code);
                 y_axis->code = y_code;
-                y_axis->name = lib_strdup(get_axis_name(y_code));
+                y_axis->name = lib_strdup(libevdev_event_code_get_name(EV_ABS, y_code));
                 if (absinfo != NULL) {
                     y_axis->minimum    = absinfo->minimum;
                     y_axis->maximum    = absinfo->maximum;
